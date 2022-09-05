7f8cb44816fce4f5a99959378e4d386558748758
mecil9 <mecil9@jcble.com>
2022-09-05 17:18:29 +0800

    day 1 complete

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+/*
+ * @Author: Mecil Meng
+ * @Date: 2022-08-29 12:23:53
+ * @LastEditors: Mecil Meng
+ * @LastEditTime: 2022-08-29 17:01:57
+ * @FilePath: /netflix-clone/components/Header.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by JCBEL/JCBLE/MSCI/MOTU, All Rights Reserved.
+ */
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+    return (
+        <header className={`${isScrolled && 'bg-[#141414]'}`}>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <img
+                    src="https://rb.gy/ulxxee"
+                    width={100}
+                    height={100}
+                    className="cursor-pointer object-contain"
+                />
+                <ul className="hidden space-x-4 md:flex">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">Tv Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New & Popular</li>
+                    <li className="headerLink">My list</li>
+                </ul>
+            </div>
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <MagnifyingGlassIcon className="hidden h-6 w-6 sm:inline" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="h-6 w-6" />
+                <Link href="/account">
+                    <img
+                        src="https://rb.gy/g1pwyx"
+                        alt=""
+                        className="cursor-pointer rounded"
+                    />
+                </Link>
+            </div>
+        </header>
+    )
+}
+
+export default Header
